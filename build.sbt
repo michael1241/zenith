f1bbb88c2c037bf7b66e40985b1e11557a5d06a7
michael1241 <michael@jamcupboard.co.uk>
2021-04-24 21:34:13 +0100

    data into Game objects

---
+++ build.sbt
@@ -0,0 +1,4 @@
+scalaVersion := "2.13.5"
+
+libraryDependencies += "joda-time" % "joda-time" % "2.10.10"
+
--- main.scala
+++ main.scala
@@ -1,23 +1,52 @@
 import scala.io.Source
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
 object Main extends App {
-    case class Game(white: String, black: String, winner: String, weektime: String, whiteelo: String, blackelo: String, whiteratingdiff: String, blackratingdiff: String, timecontrol: String)
+    case class Game(
+        white: String,
+        black: String,
+        weektime: Long,
+        whiteelo: Int,
+        blackelo: Int,
+        whiteratingdiff: Int,
+        blackratingdiff: Int,
+        timecontrol: Int
+    )
 
-    val test = Source.fromFile("test").mkString.split("\n\n\n").take(3)
+    val test = Source.fromFile("test").mkString.split("\n\n\n")
 
-    def parseGame(gamestring: String): Any = { //change from Any to Option[Game]
-        val lines: Array[String] = gamestring.split("\n")
-        lines map {
-            case s"""[${tagname} "${tagvalue}"]""" => (tagname, tagvalue) match {
-                case ("White", _) => println(tagvalue)
-                case ("Black", _) => println(tagvalue)
-                case _ => None
-            }
-            case _ => None
+    def timeConvert(control: String): Int =
+        control.split("\\+") match {
+            case Array(mins, sec) => (mins.toInt * 60) + (40 * sec.toInt)
+            case _ => 0
         }
-        
+
+    def dayConvert(date: String, time: String): Long =
+        ((DateTime.parse(time, DateTimeFormat.forPattern("HH:mm:ss").withZoneUTC()).getMillis + DateTime.parse(date, DateTimeFormat.forPattern("yyyy.MM.dd").withZoneUTC()).getMillis) / 1000) % 604800 //60*60*24*7 seconds in a week
+
+    def parseGame(gamestring: String): Option[Game] = {
+        val lines: Array[String] = gamestring.split("\n")
+        val t: Seq[String] = {
+            lines map {
+                case s"""[${tagname} "${tagvalue}"]""" => tagvalue
+            }
+        }
+        t match {
+            case t if t.length == 9 => 
+                Some(Game(white = t(0),
+                    black = t(1),
+                    weektime = dayConvert(t(2), t(3)),
+                    whiteelo = t(4).toInt,
+                    blackelo = t(5).toInt,
+                    whiteratingdiff = t(6).toInt,
+                    blackratingdiff = t(7).toInt,
+                    timecontrol = timeConvert(t(8))
+                ))
+            case _ => None
+            }
     }
 
-    test map parseGame
-
+    val games: Array[Game] = test flatMap parseGame
+    games.take(1000) map println
 }
